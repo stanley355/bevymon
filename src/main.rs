bfa421a2c41dbcba394f0d7fa92a390b063d37a7
Stanley Winata <winatastanley355@gmail.com>
2023-03-25 17:58:08 +0700

    The diff shows the addition of a new function, camera_setup, and the modification of the main function to include a call to this new function. The camera_setup function sets up a 2D camera.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(camera_setup)
         .add_startup_system(player::setup_player)
         .run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
