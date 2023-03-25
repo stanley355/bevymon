784b9669e9bd3e86d7a4c0c320f4daec97f127b2
Stanley Winata <winatastanley355@gmail.com>
2023-03-25 15:05:37 +0700

    The first file, Cargo.toml, appears to have had a line removed from it. The second file, src/main.rs, has had several lines added to it. These changes seem to be related to adding a plugin for the bevy game engine.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .run();
 }
