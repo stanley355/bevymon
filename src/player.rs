b7844fbf5fc9d9c52c9a78bec510531ad6c72c00
Stanley Winata <winatastanley355@gmail.com>
2023-03-25 18:28:18 +0700

    Can render sprites

--- src/player.rs
+++ src/player.rs
@@ -9,7 +9,8 @@ pub fn setup_player(
     mut texture_atlas_res: ResMut<Assets<TextureAtlas>>,
 ) {
     let player_sprites: Handle<Image> = asset_server.load("player_sprites.png");
-    let texture_atlas = TextureAtlas::new_empty(player_sprites, Vec2::new(0.0, 0.0));
+    let texture_atlas =
+        TextureAtlas::from_grid(player_sprites, Vec2::new(25.0, 25.0), 1, 1, None, None);
     let texture_atlas_handle = texture_atlas_res.add(texture_atlas);
 
     commands.spawn(SpriteSheetBundle {
