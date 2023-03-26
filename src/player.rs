045cff000d809f3572118a4ec8d1dce3dd20f69a
Stanley Winata <winatastanley355@gmail.com>
2023-03-26 10:11:08 +0700

    This diff contains changes to the player.rs file. The first change is the addition of the AnimationIndices struct. This struct contains information about the first and last frames of an animation. The second change is to the Player struct. The Player struct now contains a reference to an AnimationIndices struct. The third change is to the move_player function. The move_player function now takes a Query<(&Player, &AnimationIndices, &mut TextureAtlasSprite)> as an input. This query allows the function to access the player's animation information and update the sprite's index accordingly.

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct AnimationIndices {
+    pub first: usize,
+    pub last: usize,
+}
+
 #[derive(Debug, Component)]
 pub struct Player;
 
@@ -19,24 +25,46 @@ pub fn setup(
     );
     let texture_atlas_handle = texture_atlas_res.add(texture_atlas);
 
-    commands
-        .spawn(SpriteSheetBundle {
+    let animation_indices = AnimationIndices { first: 0, last: 6 };
+
+    commands.spawn((
+        SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
-            sprite: TextureAtlasSprite::new(0),
+            sprite: TextureAtlasSprite::new(animation_indices.first),
             transform: Transform::from_scale(Vec3::new(3.0, 3.0, 0.0)),
             ..default()
-        })
-        .insert(Player);
+        },
+        animation_indices,
+    ));
+
+    // commands
+    //     .spawn(SpriteSheetBundle {
+    //         texture_atlas: texture_atlas_handle,
+    //         sprite: TextureAtlasSprite::new(animation_indices.first),
+    //         transform: Transform::from_scale(Vec3::new(3.0, 3.0, 0.0)),
+    //         ..default()
+    //     })
+    //     .insert(Player);
+    // commands.spawn(animation_indices);
 }
 
 pub fn move_player(
-    mut player_entity: Query<(&Player, &mut Handle<TextureAtlas>, &mut TextureAtlasSprite,)>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_res: ResMut<Assets<TextureAtlas>>,
+    mut player_entity: Query<(&Player, &AnimationIndices, &mut TextureAtlasSprite)>,
 ) {
-
-
-    for (_player, mut text_atlas_handle, mut sprite) in player_entity.iter_mut(){
-        println!("{:?}", _player)
+    println!("hihiih");
+    for (indices, mut timer, mut sprite) in player_entity.iter_mut() {
+        sprite.index += 1;
+        println!("{:?}", sprite.index);
+        // timer.tick(time.delta());
+        // if timer.just_finished() {
+        //     sprite.index = if sprite.index == indices.last {
+        //         indices.first
+        //     } else {
+        //         sprite.index + 1
+        //     };
+        // }
     }
+    // for (_player, mut text_atlas_handle, mut sprite) in player_entity.iter_mut() {
+    //     println!("{:?}", _player)
+    // }
 }
