f47287ce164aae9772b1250c2a7d6da0889387e2
Stanley Winata <winatastanley355@gmail.com>
2023-03-27 15:57:03 +0700

    This diff contains changes to the src/main.rs file. In particular, it removes the camera_setup and tile::Tile::setup functions, and adds the window_setup function.
    The camera_setup function is responsible for setting up the camera for the game. The tile::Tile::setup function is responsible for setting up the game's tiles. The window_setup function is responsible for setting up the game's window.

--- src/main.rs
+++ src/main.rs
@@ -20,34 +20,18 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(win_plugin)) // prevent blurry sprite
         .add_plugin(WorldInspectorPlugin::new())
-        .add_startup_system(camera_setup)
-        .add_plugin(player::PlayerPlugin)
-        .add_startup_system(tile::Tile::setup)
+        // .add_startup_system(camera_setup)
+        // .add_plugin(player::PlayerPlugin)
+        // .add_startup_system(tile::Tile::setup)
         .run();
 }
 
-fn camera_setup(mut commands: Commands, query: Query<&Window>) {
+fn window_setup(mut commands: Commands, query: Query<&Window>) {
     let window = query.single();
-    println!("{:?}", window.width());
-
-    let camera = Camera {
-        viewport: Some(bevy::render::camera::Viewport {
-            physical_position: UVec2::new(500, 100),
-            physical_size: UVec2::new(1200, 800),
-            ..default()
-        }),
-        ..default()
-    };
-
-    let camera_bundle = Camera2dBundle {
-        camera,
-        ..default()
-    };
-
-    let border_bundle = NodeBundle {
+    let left_border = NodeBundle {
         style: Style {
-            size: Size::new(Val::Px(1200.), Val::Px(800.)),
-            margin: UiRect::all(Val::Px(100.)),
+            size: Size::new(Val::Px(0.1 * window.width()), Val::Px(800.)),
+            // margin: UiRect::all(Val::Px(100.)),
             ..Default::default()
         },
         transform: Transform::from_xyz(0., 0., 0.),
@@ -55,6 +39,36 @@ fn camera_setup(mut commands: Commands, query: Query<&Window>) {
         ..Default::default()
     };
 
-    commands.spawn(camera_bundle);
+    commands.spawn(left_border);
+}
+
+fn camera_setup(mut commands: Commands, query: Query<&Window>) {
+
+    // let camera = Camera {
+    //     viewport: Some(bevy::render::camera::Viewport {
+    //         physical_position: UVec2::new(500, 100),
+    //         physical_size: UVec2::new(1200, 800),
+    //         ..default()
+    //     }),
+    //     ..default()
+    // };
+
+    // let camera_bundle = Camera2dBundle {
+    //     camera,
+    //     ..default()
+    // };
+
+    let border_bundle = NodeBundle {
+        style: Style {
+            size: Size::new(Val::Px(0.1 * window.width()), Val::Px(800.)),
+            // margin: UiRect::all(Val::Px(100.)),
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0., 0., 0.),
+        background_color: Color::WHITE.into(),
+        ..Default::default()
+    };
+
+    // commands.spawn(Camera2dBundle::default());
     commands.spawn(border_bundle);
 }
