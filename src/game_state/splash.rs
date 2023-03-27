42e28cd4e000d409ce43560e358a8f12b8c1d3a0
Stanley Winata <winatastanley355@gmail.com>
2023-03-27 20:24:31 +0700

    The first thing to note is that the diff is for multiple files. The main file being changed is src/game_state/splash.rs, but there are also changes to other files in the same directory.
    The diff shows that the splash screen plugin is being changed to use a new SplashScreen struct. This struct has a start() function which sets up the splash screen image and a countdown() function which handles the timer for the splash screen. The despawn() function is called when the splash screen is finished.
    
    In the src/game_state/splash.rs file, the changes are mostly just renaming the functions to match the new SplashScreen struct. There are also some changes to the size and position of the splash screen image.

--- src/game_state/splash.rs
+++ src/game_state/splash.rs
@@ -7,9 +7,9 @@ pub struct SplashPlugin;
 
 impl Plugin for SplashPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(splash_setup)
-            .add_system(splash_countdown)
-            .add_system(splash_setup_2.in_schedule(OnExit(GameState::Splash)));
+        app.add_startup_system(SplashScreen::start)
+            .add_system(SplashScreen::countdown)
+            .add_system(SplashScreen::despawn.in_schedule(OnExit(GameState::Splash)));
     }
 }
 
@@ -19,50 +19,45 @@ struct SplashTimer(Timer);
 #[derive(Component, Debug)]
 struct SplashScreen;
 
-fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>, query: Query<&Window>) {
-    let window = query.single();
+impl SplashScreen {
+    fn start(mut commands: Commands, asset_server: Res<AssetServer>, query: Query<&Window>) {
+        let window = query.single();
 
-    let splash_img = asset_server.load("splash.png");
-    let bundle = ImageBundle {
-        style: Style {
-            // This will set the logo to be 200px wide, and auto adjust its height
-            size: Size::new(Val::Px(window.width()), Val::Auto),
+        let splash_img = asset_server.load("splash.png");
+        let size = Size::new(Val::Px(window.width() / 2.), Val::Px(window.height() / 2.));
+        let margin = UiRect {
+            left: Val::Percent(25.),
+            top: Val::Px(window.height() / 4.),
+            ..Default::default()
+        };
+
+        let bundle = ImageBundle {
+            style: Style {
+                size,
+                margin,
+                ..default()
+            },
+            image: UiImage::new(splash_img),
             ..default()
-        },
-        image: UiImage::new(splash_img),
-        ..default()
-    };
+        };
 
-    commands.spawn((bundle, SplashScreen));
-    commands.insert_resource(SplashTimer(Timer::from_seconds(3., TimerMode::Once)));
-}
-
-fn splash_countdown(
-    mut game_state: ResMut<NextState<GameState>>,
-    time: Res<Time>,
-    mut timer: ResMut<SplashTimer>,
-) {
-    let timer_tick = timer.tick(time.delta());
-    if timer_tick.finished() {
-        game_state.set(GameState::Menu);
+        commands.spawn((bundle, SplashScreen));
+        commands.insert_resource(SplashTimer(Timer::from_seconds(3., TimerMode::Once)));
     }
-}
 
-fn splash_setup_2(mut commands: Commands, asset_server: Res<AssetServer>, query: Query<&Window>, query2: Query<(Entity, &SplashScreen)>) {
-    let window = query.single();
-    let screen = query2.single();
+    fn countdown(
+        mut game_state: ResMut<NextState<GameState>>,
+        time: Res<Time>,
+        mut timer: ResMut<SplashTimer>,
+    ) {
+        let timer_tick = timer.tick(time.delta());
+        if timer_tick.finished() {
+            game_state.set(GameState::Menu);
+        }
+    }
 
-    let splash_img = asset_server.load("tiles.png");
-    let bundle = ImageBundle {
-        style: Style {
-            // This will set the logo to be 200px wide, and auto adjust its height
-            size: Size::new(Val::Px(window.width()), Val::Auto),
-            ..default()
-        },
-        image: UiImage::new(splash_img),
-        ..default()
-    };
-
-    commands.get_entity(screen.0).unwrap().despawn();
-    commands.spawn(bundle);
+    fn despawn(mut commands: Commands, query: Query<(Entity, &SplashScreen)>) {
+        let screen = query.single();
+        commands.get_entity(screen.0).unwrap().despawn();
+    }
 }
