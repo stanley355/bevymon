4ee16095d504c5372e4400d410d5d95b0b17defc
Stanley Winata <winatastanley355@gmail.com>
2023-03-30 16:49:11 +0700

    Insert story state

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,9 @@ fn main() {
         .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(camera_setup)
         .add_plugin(TweeningPlugin)
-        .add_plugins(startup::StartupPluginGroup)
         .add_plugin(chat::plugin::ChatPlugin)
         .add_plugins(story::plugin::StoryPluginGroup)
+        .add_plugins(startup::StartupPluginGroup)
         .run();
 }
 
--- src/story/plugin.rs
+++ src/story/plugin.rs
@@ -2,12 +2,24 @@ use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
 
 use super::chapter_one::plugin::ChapterOnePlugin;
+use super::state::StoryState;
 
 #[derive(Debug)]
 pub struct StoryPluginGroup;
 
 impl PluginGroup for StoryPluginGroup {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>().add(ChapterOnePlugin)
+        PluginGroupBuilder::start::<Self>()
+            .add(StoryPlugin)
+            .add(ChapterOnePlugin)
+    }
+}
+
+#[derive(Debug)]
+pub struct StoryPlugin;
+
+impl Plugin for StoryPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_state::<StoryState>();
     }
 }
