efc794bb3bc7a88773cc55dcc20f9f76197dedfb
Stanley Winata <winatastanley355@gmail.com>
2023-03-30 16:32:13 +0700

    Chapeter one sate

--- src/story/chapter_one/plugin.rs
+++ src/story/chapter_one/plugin.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+use super::scene::BackgroundScene;
 use super::state::ChapterOneState;
 
 #[derive(Debug)]
@@ -7,6 +8,7 @@ pub struct ChapterOnePlugin;
 
 impl Plugin for ChapterOnePlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<ChapterOneState>();
+        app.add_state::<ChapterOneState>()
+            .add_system(BackgroundScene::new.in_schedule(OnEnter(ChapterOneState::BackgroundScene)));
     }
 }
--- src/story/chapter_one/scene.rs
+++ src/story/chapter_one/scene.rs
@@ -1,15 +1,23 @@
 use bevy::prelude::*;
 
-use crate::chat::resource::ChatResource;
+use crate::chat::{resource::ChatResource, state::ChatState};
 
 #[derive(Debug, Component)]
 pub struct BackgroundScene;
 
 impl BackgroundScene {
-   pub fn  new(
+    pub fn new(
         mut chat_res_mut: ResMut<ChatResource>,
-
+        mut chat_state: ResMut<NextState<ChatState>>,
     ) {
-        chat_res_mut.dialogues = vec!["At the beginning there was creation".to_string(), "But where does creation comes from?".to_string()]
+        chat_res_mut.dialogues = vec![
+            "At the beginning there was creation".to_string(),
+            "But where does creation comes from?".to_string(),
+            "Some said it started from egg".to_string(),
+            "Some said it started from Mew".to_string(),
+            "Some said it started from Arceus".to_string(),
+            "Nobody knows...".to_string(),
+        ];
+        chat_state.set(ChatState::OnChat);
     }
 }
