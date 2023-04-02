c97ad6432f3877c8cdf97ccee7af88406136f4ac
Stanley Winata <winatastanley355@gmail.com>
2023-04-02 10:11:09 +0700

    Tilemap components

--- src/tilemap/components.rs
+++ src/tilemap/components.rs
@@ -4,20 +4,30 @@ use bevy::prelude::*;
 pub struct Tile;
 
 impl Tile {
-    pub fn texture_atlas_handle(
+    pub fn texture_atlas(
         asset_server: Res<AssetServer>,
         mut texture_atlas_res: ResMut<Assets<TextureAtlas>>,
     ) -> Handle<TextureAtlas> {
         let tile_image: Handle<Image> = asset_server.load("images/tiles/dis_world_tiles.png");
 
-        let texture_atlas = TextureAtlas::from_grid(
-            tile_image,
-            Vec2::new(20.0, 20.0),
-            9,
-            7,
-            None,
-            None,
-        );
+        let texture_atlas =
+            TextureAtlas::from_grid(tile_image, Vec2::new(20.0, 20.0), 9, 7, None, None);
         return texture_atlas_res.add(texture_atlas);
+    }
+
+    pub fn spawn(
+        mut commands: Commands,
+        texture_atlas: Handle<TextureAtlas>,
+        sprite_index: usize,
+        transform: Transform,
+    ) {
+        let bundle = SpriteSheetBundle {
+            texture_atlas: texture_atlas.clone(),
+            sprite: TextureAtlasSprite::new(sprite_index),
+            transform,
+            ..default()
+        };
+
+        commands.spawn(bundle);
     }
 }
